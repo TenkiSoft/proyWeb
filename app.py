3b5013fba09d415ec6c34ef66da7026d76813895
Edward Gonzalez <claiunocode@gmail.com>
2025-05-15 18:24:31 -0500

    Base

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from content.utils.sections import SectionContent
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    Sections = SectionContent()
+    content_pages = Sections.get_content()
+    return render_template('index.html',content_pages=content_pages)
+if __name__ == '__main__':
+    app.run(debug=True)
